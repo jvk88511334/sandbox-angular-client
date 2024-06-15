7d9d1fdbcd7ece10c18e5ac62e693b08bc60ec43
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-06-15 14:10:00 +0200

    Configurer le routing dans app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Vue1Component } from './vue1/vue1.component';
+import { Vue2Component } from './vue2/vue2.component';
+import { Vue3Component } from './vue3/vue3.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'vue1', component: Vue1Component },
+  { path: 'vue2', component: Vue2Component },
+  { path: 'vue3', component: Vue3Component },
+  { path: '', redirectTo: '/vue1', pathMatch: 'full' }
+];
