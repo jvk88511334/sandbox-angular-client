74a5278f6b572819da1138eed2954ec414f05392
Jerome Villiseck <jerome.villiseck@outlook.com>
2024-06-15 14:39:39 +0200

    Imports dans le composant header des éléments necessaires au template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MatToolbar
+    MatToolbar,
+    MatButton,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
